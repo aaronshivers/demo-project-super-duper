949690d24d261f6d762d69ee3f76babe689db1b4
aaronshivers <aaron@moonmail.org>
2019-07-21 17:42:24 -0500

    changed styling added some links

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {
+  Container,
   Navbar,
   Nav,
   NavDropdown,
@@ -10,33 +11,42 @@ import {
 import logo from '../assets/logo.png'
 
 const NavbarComponent = () => (
-  <Navbar bg="light" expand="lg">
-    <Navbar.Brand href="#home">
-      <img
-        alt="Demo Project"
-        src={ logo }
-        height="30"
-        className="d-inline-block align-top"
-      />
-    </Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mr-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">Link</Nav.Link>
-        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-        </NavDropdown>
-      </Nav>
-      <Form inline>
-        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-        <Button variant="outline-success">Search</Button>
-      </Form>
-    </Navbar.Collapse>
+  <Navbar bg="dark" variant="dark" expand="lg">
+    <Container>
+      <Navbar.Brand href="#home">
+        <img
+          alt="Demo Project"
+          src={ logo }
+          height="30"
+          className="d-inline-block align-top"
+        />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#link">Services</Nav.Link>
+          <Nav.Link href="#link">Blog</Nav.Link>
+          <Nav.Link href="#link">Case Studies</Nav.Link>
+          <Nav.Link href="#link">Shop</Nav.Link>
+          <Nav.Link href="#link">Contact Us</Nav.Link>
+          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Home</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Services</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Blog</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.4">Case Studies</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.5">Shop</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.6">Contact Us</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.7">Separated link</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="primary">Search</Button>
+        </Form>
+      </Navbar.Collapse>
+    </Container>
   </Navbar>
 )
 
