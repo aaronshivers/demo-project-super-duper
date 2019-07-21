7b02245e73cb44b03a6f28ae74e84b3a55ca73c8
aaronshivers <aaron@moonmail.org>
2019-07-21 17:08:43 -0500

    added loadingPage component

--- src/app.js
+++ src/app.js
@@ -1,7 +1,19 @@
 import React from 'react'
 import { render } from 'react-dom'
-import App from './components/App'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
+import App from './components/App'
+import LoadingPage from './components/LoadingPage'
 
-render(<App />, document.getElementById('app'))
+
+let hasRendered = false
+const renderApp = () => {
+  if (!hasRendered) {
+    render(<App />, document.getElementById('app'))
+    hasRendered = true
+  }
+}
+
+render(<LoadingPage />, document.getElementById('app'))
+
+setTimeout(() => renderApp(), 1000)
